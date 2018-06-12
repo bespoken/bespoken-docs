ee1ac61c82ba8357c9281d574bba2fd6de53387c
John Kelvie <jpk@bespoken.io>
2018-06-12 14:54:48 -0500

    Cleaning up tutorials and accordion

--- doks-theme/assets/js/accordion.js
+++ doks-theme/assets/js/accordion.js
@@ -2,9 +2,9 @@ $( function() {
     $( ".accordion-button" ).click(function () {
         var clickedId = "#" + $(this).attr('data-target');
         if ($(clickedId).is(":visible")) {
-            $(clickedId).hide("slow");
+            $(clickedId).hide();
         } else {
-            $(clickedId).show("slow");
+            $(clickedId).show();
         }       
     });
 
