e6475cb72fbb87aa27957c060decc34f68958568
Juan Perata <juan@bespoken.tools>
2019-10-31 10:47:33 -0500

    Add Algolia API-Key for the search bar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,10 @@ module.exports = {
     title: "Bespoken Docs",
     descripcion: "this is a great job",
     themeConfig: {
+        algolia: {
+            apiKey: '36d68ce99344a644d0bc7667b5ee4003',
+            indexName: 'bespoken'
+        },
         bespokenUri: "https://bespoken.io/",
         logo: "/assets/images/BespokenLogo-small.png",
         head: [
